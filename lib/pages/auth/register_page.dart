809c8e0af2bbb449f28b3fb1ca411987dbc20637
Abhishek <abhishakeappy@gmail.com>
2022-12-31 18:42:58 +0545

    Register page design

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_practise/pages/auth/register_page.dart';
-import 'package:flutter_practise/shared/constants.dart';
-import 'package:flutter_practise/widgets/widgets.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+import '../../widgets/widgets.dart';
+import 'login_page.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   bool _obscureText = true;
   final formKey = GlobalKey<FormState>();
-
   String email = "";
   String password = "";
+  String fullName = "";
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +37,7 @@ class _LoginPageState extends State<LoginPage> {
                           color: Color(0xff075E54))),
                   const SizedBox(height: 10),
                   const Text(
-                      "Log in now and bring the world closer together, one conversation at a time",
+                      "Create your account now to see what people are talking about!",
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         fontSize: 15,
@@ -45,6 +45,24 @@ class _LoginPageState extends State<LoginPage> {
                         color: Colors.grey,
                       )),
                   Image.asset("assets/main.png"),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(
+                      prefixIcon:
+                          const Icon(Icons.person, color: Color(0xff075E54)),
+                      labelText: 'Full Name',
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        fullName = val;
+                      });
+                    },
+                    validator: (val) {
+                      return val!.isNotEmpty 
+                          ? null
+                          : "Name cannot be empty!!";
+                    },
+                  ),
+                  const SizedBox(height: 15),
                   TextFormField(
                     decoration: textInputDecoration.copyWith(
                       prefixIcon:
@@ -114,12 +132,12 @@ class _LoginPageState extends State<LoginPage> {
                       child: const Padding(
                         padding: EdgeInsets.all(15.0),
                         child: Text(
-                          "Login",
+                          "Register",
                           style: TextStyle(color: Colors.white, fontSize: 16),
                         ),
                       ),
                       onPressed: () {
-                        login();
+                        register();
                       },
                     ),
                   ),
@@ -127,17 +145,17 @@ class _LoginPageState extends State<LoginPage> {
                     height: 20,
                   ),
                   Text.rich(TextSpan(
-                    text: "Don't have an account? ",
+                    text: "Already have an account? ",
                     style: const TextStyle(color: Colors.black, fontSize: 14),
                     children: <TextSpan>[
                       TextSpan(
-                          text: "Register here",
+                          text: "Login",
                           style: const TextStyle(
                               color: Colors.black,
                               decoration: TextDecoration.underline),
                           recognizer: TapGestureRecognizer()
                             ..onTap = () {
-                              nextScreen(context, const RegisterPage());
+                              nextScreen(context, const LoginPage());
                             }),
                     ],
                   )),
@@ -148,7 +166,7 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  login() {
+  register() {
     if (formKey.currentState!.validate()) {}
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_practise/shared/constants.dart';
 
 const textInputDecoration = InputDecoration(
-  labelStyle: TextStyle(color: Colors.black, ),
-  focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Color(0xff075E54), width: 2.0),
-  ), 
-  enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Color.fromARGB(255, 174, 196, 186), width: 2.0),
-  ),
-   errorBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.red, width: 2.0),
-   )
-);
+    labelStyle: TextStyle(
+      color: Colors.black,
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Color(0xff075E54), width: 2.0),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide:
+          BorderSide(color: Color.fromARGB(255, 174, 196, 186), width: 2.0),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.red, width: 2.0),
+    ));
+
+void nextScreen(context, page) {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+}
+
+void nextScreenReplace(context, page) {
+  Navigator.pushReplacement(
+      context, MaterialPageRoute(builder: (context) => page));
+}
