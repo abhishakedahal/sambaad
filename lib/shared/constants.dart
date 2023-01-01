dd3e698b987e38bd103994b6458bd6c0330ceb90
Abhishek <abhishakeappy@gmail.com>
2023-01-01 17:26:59 +0545

    updated firebase constants and packages

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static String apiKey = "AIzaSyAP3_8-vRlKmKbntic1w7_2md_a7-StTjY";
-  static String authDomain = "chatappsambaad.firebaseapp.com";
-  static String projectId = "chatappsambaad";
-  static String storageBucket = "chatappsambaad.appspot.com";
-  static String messagingSenderId = "212800799431";
-  static String appId = "1:212800799431:web:7ad35dcc45e66c1fb2f4e2";
+  static String apiKey = "AIzaSyDmodsjykTIHMTt-zxDPcK3Jv33KgOhhnk";
+  static String authDomain = "sambaad-72d35.firebaseapp.com";
+  static String projectId = "sambaad-72d35";
+  static String storageBucket = "sambaad-72d35.appspot.com";
+  static String messagingSenderId = "309225266866";
+  static String appId = "1:309225266866:web:f16b95321c4b1f03b5b3ab";
   final primaryColor = const Color(0xff075E54);
 }
