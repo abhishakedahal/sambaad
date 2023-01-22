4395787a50f0a183146ccf9eb7926ddd4f772c2c
Kamal Shrestha <kamalshrestha583@gmail.com>
2023-01-22 14:07:01 +0545

    member id is added

--- lib/pages/group_info.dart
+++ lib/pages/group_info.dart
@@ -176,7 +176,7 @@ class _GroupInfoState extends State<GroupInfo> {
                         ),
                       ),
                       title: Text(getName(snapshot.data['members'][index])),
-                      
+                      //subtitle: Text(getId(snapshot.data['members'][index])),
                     ),
                   );
                 },
