a277058d2ce13074e775e1b1ad3f2c6532839b38
Abhishek <abhishakeappy@gmail.com>
2023-02-07 20:54:14 +0545

    updated configs for kushal

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 class Constants{
-  static String appId="1:349620511454:web:d39dc5bcdcf44f0ad6584f";
-  static String apiKey="AIzaSyBy6c7mSLW8A54OwMtezLQrqPZe6sW9QDE";
-  static String messagingSenderId="349620511454";
-  static String projectId="sambaad-4";
+  static String appId="1:641169153542:web:6e355e227f0e6b3ef0ad87";
+  static String apiKey="AIzaSyAt97pPJoPWynh6BA2vJ9LgNpZuUtUa6vY";
+  static String messagingSenderId="641169153542";
+  static String projectId="sambaad-kushal";
   //final primaryColor=const Color(0xFFee7b64);
   // final primaryColor=const Color(0xff075E54);
   // final primaryColor=Color.fromARGB(255, 50, 155, 253);
