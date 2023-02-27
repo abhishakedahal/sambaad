b4f68a1e34cd224a768905bacac00a60b58e6daf
Kamal Shrestha <kamalshrestha583@gmail.com>
2023-02-27 23:19:39 +0545

    checking recent message only

--- lib/widgets/group_title.dart
+++ lib/widgets/group_title.dart
@@ -87,7 +87,7 @@ String recentMessage = "";
             style: const TextStyle(fontWeight: FontWeight.bold),
           ),
           subtitle:
-          recentMessageSender == "" && recentMessage == ""
+           recentMessage == ""
               ? const Text("No recent messages")
               :
            Text(
